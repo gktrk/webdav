2c65725ce3e81ded4f3fdd3b059cf3002f578fa8
Göktürk Yüksek <gokturk@gentoo.org>
2020-11-21 00:29:56 -0500

    Add support for TOTP over basic auth
    Instead of using a static password, allow a TOTP token to be specified
    as the password. The interesting side effect of this is that the
    resources will be available only for the duration of the TOTP
    period. Applications that cache the password for future accesses may
    break. However, this also establishes a "natural" timeout as well.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/pelletier/go-toml v1.6.0 // indirect
+	github.com/pquerna/otp v1.3.0
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
--- lib/utils.go
+++ lib/utils.go
@@ -4,12 +4,21 @@ import (
 	"strings"
 
 	"golang.org/x/crypto/bcrypt"
+	"github.com/pquerna/otp"
+	"github.com/pquerna/otp/totp"
 )
 
 func checkPassword(saved, input string) bool {
 	if strings.HasPrefix(saved, "{bcrypt}") {
 		savedPassword := strings.TrimPrefix(saved, "{bcrypt}")
 		return bcrypt.CompareHashAndPassword([]byte(savedPassword), []byte(input)) == nil
+	} else if strings.HasPrefix(saved, "{otpauth}") {
+		otp_string := strings.TrimPrefix(saved, "{otpauth}")
+		if key,_ := otp.NewKeyFromURL(otp_string); key != nil {
+			if totp.Validate(input, key.Secret()) {
+				return true
+			}
+		}
 	}
 
 	return saved == input
